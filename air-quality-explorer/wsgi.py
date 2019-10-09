f6d1aea5c112d13eaa0c33049a56e81deac9ad73
Thannarot Kunlamai <thannarotk@gmail.com>
2019-10-09 19:37:08 +0700

    change wsgi application

--- air-quality-explorer/wsgi.py
+++ air-quality-explorer/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hydrafloodviewer project.
+WSGI config for air-quality-explorer project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hydrafloodviewer.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "air-quality-explorer.settings")
 
 application = get_wsgi_application()
