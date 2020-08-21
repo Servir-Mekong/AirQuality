b1fb39d4e2a83ab916c8b6ae69c412dd250260e3
NDC\gtondapu <githika.tondapu@nasa.gov>
2020-08-21 14:02:53 -0500

    updated script

--- downloadGEOSData/weeklyForecastCheck.sh
+++ downloadGEOSData/weeklyForecastCheck.sh
@@ -1,33 +1,26 @@
-declare -a lastWeek=("$(date +"%Y%m%d")" "$(date -d '-1 day' '+%Y%m%d')" "$(date -d '-2 day' '+%Y%m%d')"
+declare -a lastWeek=("$(date '+%Y%m%d')" "$(date -d '-1 day' '+%Y%m%d')" "$(date -d '-2 day' '+%Y%m%d')"
 "$(date -d '-3 day' '+%Y%m%d')" "$(date -d '-4 day' '+%Y%m%d')" "$(date -d '-5 day' '+%Y%m%d')" "$(date -d '-6 day' '+%Y%m%d')")
-#declare -a lastWeek=("$(date +"%Y%m%d")")
-declare -a test
+set -a missedDownloads
 
-cd /home/tethys/aq_dir/geos/
-yourfilenames=`ls *.nc`
-j=0
-for eachfile in $yourfilenames
+cd /mnt/mk_aqx/geos/
+downloadedFiles=`ls *.nc`
+missedCount=0
+for file in $downloadedFiles
 do
-    for i in "${lastWeek[@]}" ; do
+    for singledate in "${lastWeek[@]}" ; do
 
-        if [ "$eachfile" == "$i.nc" ]
-            then echo "executing your bash script file"
-            #./myscript.sh
-        else
-            echo $j
-            test[$j]=$i
-            j=$(( $j + 1 ))
-
+        if [ "$file" -ne "$singledate.nc" ]
+            missedDownloads[$j]=$singledate
+            missedCount=$(( $missedCount + 1 ))
         fi
-
     done
 done
-if [ $j -ge 0 ]
+if [ $missedCount -ge 0 ]
     then
-      for day in "${test[@]}" ;
+      for day in "${missedDownloads[@]}" ;
        do
         echo $(date -d $day +'%Y-%m-%d')
-        cd /home/tethys/tethyssco/AirQuality-downloadGEOSData/downloadGEOSData/
-        sh combine.sh $(date -d $day +'%Y-%m-%d')
+        cd /home/tethys/scripts/AirQuality/downloadGEOSData/
+        sh combine.sh "$(date -d $day +'%Y-%m-%d')"
       done
 fi
